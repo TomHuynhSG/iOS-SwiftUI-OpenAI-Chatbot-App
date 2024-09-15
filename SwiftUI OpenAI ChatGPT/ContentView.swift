1d3abac9b2d19a85696576c9ce267c45fa47737f
Tom Huynh <tom.huynh@rmit.edu.vn>
2024-09-15 20:34:35 +0700

    Initial Commit

---
+++ SwiftUI OpenAI ChatGPT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI OpenAI ChatGPT
+//
+//  Created by Tom Huynh on 15/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI OpenAI ChatGPT/SwiftUI_OpenAI_ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_OpenAI_ChatGPTApp.swift
+//  SwiftUI OpenAI ChatGPT
+//
+//  Created by Tom Huynh on 15/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_OpenAI_ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
